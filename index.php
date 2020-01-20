b4d53792af206dff7f4f01be4a5ad26e0ab4e1b5
Cody Zipp <Czipp2@mail.greenriver.edu>
2020-01-20 17:29:47 -0500

    added the image, changed the index page, and finished all html code

--- index.php
+++ index.php
@@ -1,15 +1,22 @@
 <?php
-?>
-<!doctype html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Index page</title>
-</head>
-<body>
+//this is our controller!
 
-</body>
-</html>
+//turn on error reporting
+ini_set('display_errors', 1);
+error_reporting(E_ALL);
+
+//require the autoload file
+require_once ('vendor/autoload.php');
+
+//create an instance of the base class
+$f3 = Base::instance();
+
+//define a default route
+$f3->route('GET /', function() {
+    $view = new Template();
+    echo $view -> render('views/home.html');
+});
+
+//run fat free
+$f3 -> run();
+
