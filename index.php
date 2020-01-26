b879cc1546621fc771a959e58d32284c541da747
Cody Zipp <Czipp2@mail.greenriver.edu>
2020-01-26 18:13:02 -0500

    added most of the results and updated index page

--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <?php
 //this is our controller!
 
+session_start();
 //turn on error reporting
 ini_set('display_errors', 1);
 error_reporting(E_ALL);
@@ -25,20 +26,41 @@ $f3->route('GET /profile', function() {
 
 //define a form2 route
 $f3->route('POST /profile2', function() {
+    var_dump($_POST);
+    $_SESSION['first'] = $_POST['first'];
+    $_SESSION['last'] = $_POST['last'];
+    $_SESSION['age'] = $_POST['age'];
+    $_SESSION['gender'] = $_POST['gender'];
+    $_SESSION['phone'] = $_POST['phone'];
     $view = new Template();
     echo $view -> render('views/form2.html');
 });
 
 //define a form3 route
 $f3->route('POST /profile3', function() {
+    var_dump($_POST);
+    $_SESSION['email'] = $_POST['email'];
+    $_SESSION['state'] = $_POST['state'];
+    $_SESSION['seeking'] = $_POST['seeking'];
+    $_SESSION['bio'] = $_POST['bio'];
     $view = new Template();
     echo $view -> render('views/form3.html');
 });
 
 //define a summary route
 $f3 -> route('POST /summary', function() {
-//    var_dump($_POST);
-//    $_SESSION['order'] = $_POST['food'];
+    var_dump($_POST);
+    $string = "";
+    for($i = 0; $i < count($_POST['indoor']); $i++) {
+        $string .= $_POST['indoor'][$i]." ";
+    }
+    $_SESSION['indoor'] = $string;
+
+    $string1 = "";
+    for ($j = 0; $j < count($_POST['outdoor']); $j++) {
+        $string1 .= $_POST['outdoor'][$i]." ";
+    }
+    $_SESSION['outdoor'] = $string1;
     $view = new Template();
     echo $view -> render('views/results.html');
 });
