88fa5d31f82eca441560be73fa0be1003f90bb0c
Cody Zipp <Czipp2@mail.greenriver.edu>
2020-01-26 13:46:53 -0500

    finished form 1 and updated index page

--- index.php
+++ index.php
@@ -17,6 +17,17 @@ $f3->route('GET /', function() {
     echo $view -> render('views/home.html');
 });
 
+//define a profile route
+$f3->route('GET /profile', function() {
+    $view = new Template();
+    echo $view -> render('views/form1.html');
+});
+
+//define a form2 route
+$f3->route('POST /profile2', function() {
+    $view = new Template();
+    echo $view -> render('views/form2.html');
+});
 //run fat free
 $f3 -> run();
 
